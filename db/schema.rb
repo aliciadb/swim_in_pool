64822954d131294a3a8c2bbd2dab13170a5e078d
Antonia Lappin <antonia.lappin@gmx.de>
2022-05-23 16:26:13 +0200

    bookings model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2022_05_23_134244) do
-=======
-ActiveRecord::Schema[7.0].define(version: 2022_05_23_134435) do
->>>>>>> 415e81ad08a4f2ab94026186e0344be8d34e5d05
+ActiveRecord::Schema[7.0].define(version: 2022_05_23_142506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.float "price"
+    t.bigint "user_id", null: false
+    t.bigint "pool_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pool_id"], name: "index_bookings_on_pool_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "pools", force: :cascade do |t|
     t.string "name"
@@ -47,5 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_23_134435) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "pools"
+  add_foreign_key "bookings", "users"
   add_foreign_key "pools", "users"
 end
