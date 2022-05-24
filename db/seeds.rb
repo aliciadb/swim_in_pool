f04e916830f969042d9df6bdae930fe5715cb9b3
Natalie Rauh <97293748+Nat-Ma@users.noreply.github.com>
2022-05-24 11:21:11 +0200

    changes to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-require 'faker'
 
-puts "Create user database"
+puts "Create pools database"
 User.delete_all
 
 10.times do
-  user = User.new(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
-    password: 123456,
-    profile_image: 'https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070'
+  Pool.new(
+    name: ,
+    rating: ,
+    description: ,
+    price: ,
+    location: ,
+    image: ,
+    category:
+    user:
   )
-  puts "#{user.first_name} #{user.last_name}"
 end
