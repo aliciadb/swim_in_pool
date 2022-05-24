6ed8717abb4bfac5df5325d7d744e325c3ee92e3
Antonia Lappin <antonia.lappin@gmx.de>
2022-05-24 11:59:48 +0200

    methods new and create

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -3,4 +3,20 @@ class PoolsController < ApplicationController
 
   def index
   end
+
+  def new
+    @pool = Pool.new
+  end
+
+  def create
+    @pool = Pool.new(params[:pool])
+    @pool.save
+    redirect_to pool_path(@pool)
+  end
+
+  private
+
+  def pool_params
+    params.require(:pool).permit(:name, :description, :price, :image, :category)
+  end
 end
