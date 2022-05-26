08d7051c2f52a38a2fde12f17e68632769020e90
PiaPoklekowski <poklekowskipia@gmail.com>
2022-05-26 12:07:43 +0200

    Backend for search

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pools#index"
+  root "pages#home"
   delete "pools/:id", to: "pools#destroy", as: :delete_pool
 
-  resources :pools, except: [:index, :destroy] do
+  resources :pools, except: [:destroy] do
     resources :bookings, only: [:new, :create]
   end
   get "dashboards", to: "dashboards#show"
